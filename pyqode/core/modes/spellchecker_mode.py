6214b2131abe68297735110bb14cceaa26cb7b0e
Sebastiaan Mathot <s.mathot@cogsci.nl>
2020-01-13 15:51:01 +0100

    SpellCheckerMode: include single quotes as parts of words

--- pyqode/core/modes/spellchecker_mode.py
+++ pyqode/core/modes/spellchecker_mode.py
@@ -8,7 +8,7 @@ from pyqode.core.modes import CheckerMode
 WARNING = 1
 # Matches all words of at least three characters that are preceded whitespace
 # or opening brackets
-WORD_PATTERN = r'[\s\[\(\{](?P<word>\w\w(\w+))'
+WORD_PATTERN = r'[\s\[\(\{](?P<word>[\w\'][\w\']([\w\']+))'
 
 
 def run_spellcheck(request_data):
