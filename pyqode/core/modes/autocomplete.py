7e1c152f7dcad53fab1b4ed3d4105088e0b43030
smathot <s.mathot@cogsci.nl>
2020-05-03 16:09:52 +0200

    AutoCompleteMode: avoid duplicating quote characters

--- pyqode/core/modes/autocomplete.py
+++ pyqode/core/modes/autocomplete.py
@@ -24,7 +24,10 @@ class AutoCompleteMode(Mode):
         self.MAPPING = {'"': '"', "'": "'", "(": ")", "{": "}", "[": "]"}
         self.AVOID_DUPLICATES = ')', ']', '}'
         #: The format to use for each symbol in mapping when there is a selection
-        self.SELECTED_QUOTES_FORMATS = {key: '%s%s%s' for key in self.MAPPING.keys()}
+        self.SELECTED_QUOTES_FORMATS = {
+            key: '%s%s%s'
+            for key in self.MAPPING.keys()
+        }
         #: The format to use for each symbol in mapping when there is no selection
         self.QUOTES_FORMATS = {key: '%s' for key in self.MAPPING.keys()}
         self.logger = logging.getLogger(__name__)
@@ -37,63 +40,68 @@ class AutoCompleteMode(Mode):
         else:
             self.editor.post_key_pressed.disconnect(self._on_post_key_pressed)
             self.editor.key_pressed.disconnect(self._on_key_pressed)
+        self._helper = TextHelper(self.editor)
 
     def _on_post_key_pressed(self, event):
-        if not event.isAccepted() and not self._ignore_post:
-            txt = event.text()
-            trav = self.editor.textCursor()
-            assert isinstance(trav, QtGui.QTextCursor)
-            trav.movePosition(trav.Left, trav.MoveAnchor, 2)
-            literal = TextHelper(self.editor).is_comment_or_string(trav)
-            if not literal:
-                next_char = TextHelper(self.editor).get_right_character()
-                if txt in self.MAPPING:
-                    to_insert = self.MAPPING[txt]
-                    if (not next_char or next_char in self.MAPPING.keys() or
-                            next_char in self.MAPPING.values() or
-                            next_char.isspace()):
-                        TextHelper(self.editor).insert_text(
-                            self.QUOTES_FORMATS[txt] % to_insert)
-        self._ignore_post = False
+        if event.isAccepted() or self._ignore_post:
+            self._ignore_post = False
+            return
+        txt = event.text()
+        if txt not in self.MAPPING:
+            return
+        next_char = self._helper.get_right_character()
+        to_insert = self.MAPPING[txt]
+        if (not next_char or next_char in self.MAPPING.keys() or
+                next_char in self.MAPPING.values() or
+                next_char.isspace()):
+            self._helper.insert_text(
+                self.QUOTES_FORMATS[txt] % to_insert
+            )
 
     def _on_key_pressed(self, event):
         txt = event.text()
         cursor = self.editor.textCursor()
-        from pyqode.qt import QtGui
-        assert isinstance(cursor, QtGui.QTextCursor)
+        next_char = self._helper.get_right_character()
         if cursor.hasSelection():
             # quoting of selected text
-            if event.text() in self.MAPPING.keys():
-                first = event.text()
-                last = self.MAPPING[event.text()]
+            if txt in self.MAPPING.keys():
+                closing_char = self.MAPPING[txt]
+                cursor.beginEditBlock()
                 cursor.insertText(
-                    self.SELECTED_QUOTES_FORMATS[event.text()] % (
-                        first, cursor.selectedText(), last))
+                    txt + cursor.selectedText() + closing_char
+                )
+                # Don't duplocate the closing character
+                if self._helper.get_right_character(cursor) == closing_char:
+                    cursor.insertText('')
+                else:
+                    cursor.movePosition(cursor.Left, cursor.MoveAnchor, 1)
                 self.editor.setTextCursor(cursor)
+                cursor.endEditBlock()
                 event.accept()
-            else:
-                self._ignore_post = True
+            self._ignore_post = True
             return
-        next_char = TextHelper(self.editor).get_right_character()
-        self.logger.debug('next char: %s', next_char)
         ignore = False
         if event.key() == QtCore.Qt.Key_Backspace:
-            # get the character that will get deleted
-            tc = self.editor.textCursor()
-            pos = tc.position()
-            tc.movePosition(tc.Left)
-            tc.movePosition(tc.Right, tc.KeepAnchor)
-            del_char = tc.selectedText()
-            if del_char in self.MAPPING and \
-                    self.MAPPING[del_char] == next_char:
-                tc.beginEditBlock()
-                tc.movePosition(tc.Right, tc.KeepAnchor)
-                tc.insertText('')
-                tc.setPosition(pos - 2)
-                tc.endEditBlock()
-                self.editor.setTextCursor(tc)
+            # If we're in between two quotes, a backspace will delete both
+            # quotes.
+            del_char = self._helper.get_left_character()
+            if (
+                del_char in self.MAPPING and
+                self.MAPPING[del_char] == next_char
+            ):
+                # First select the previous and next character and replace it
+                # by an empty string. Then move the cursor one position back.
+                cursor.beginEditBlock()
+                cursor.movePosition(cursor.Left, cursor.MoveAnchor, 1)
+                cursor.movePosition(cursor.Right, cursor.KeepAnchor, 2)
+                cursor.insertText('')
+                cursor.movePosition(cursor.Left, cursor.MoveAnchor, 1)
+                cursor.endEditBlock()
+                self.editor.setTextCursor(cursor)
                 ignore = True
         elif (
+            # If the next character is already a character that we just
+            # autocompleted, then ignore the key press
             txt and next_char == txt and (
                 next_char in self.MAPPING or
                 txt in self.AVOID_DUPLICATES
@@ -102,5 +110,5 @@ class AutoCompleteMode(Mode):
             ignore = True
         if ignore:
             event.accept()
-            TextHelper(self.editor).clear_selection()
-            TextHelper(self.editor).move_right()
+            self._helper.clear_selection()
+            self._helper.move_right()
